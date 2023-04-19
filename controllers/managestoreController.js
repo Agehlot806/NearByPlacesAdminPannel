ad885fa49bd170304ad62c201a5de6f1b341a2eb
Adarsh_the_Developer <adarsh_the_developer@adarshs-MacBook-Pro.local>
2023-04-19 15:57:38 +0530

    more apis added

--- controllers/managestoreController.js
+++ controllers/managestoreController.js
@@ -31,8 +31,6 @@ export const AddnewStore = catchAsyncError(async (req, res, next) => {
           
     const newstoreobject = {
         name,category,phonenumber,website,details,videourl,latitude,longitude,storegallery,storephoto }
-    
-    
     store = new Store(newstoreobject);
     await store.save()
     res.status(201).json({
@@ -40,3 +38,74 @@ export const AddnewStore = catchAsyncError(async (req, res, next) => {
       message: "Store added Successfully.",
     });
   });
+
+  export const GetAllStores  = catchAsyncError(async (req, res, next) => {
+    const stores = await Store.find({});
+    res.status(200).json({
+      success: true,
+      stores,
+    });
+  });
+
+  
+export const DeleteStore = catchAsyncError(async (req, res, next) => {
+    const store = await Store.findById(req.params.id);
+    if (!store) return next(new ErrorHandler("Store not found", 404));
+    await cloudinary.v2.uploader.destroy(store.storegallery.public_id);
+    await cloudinary.v2.uploader.destroy(store.storephoto.public_id);
+   await store.deleteOne();
+    res.status(200).json({
+      success: true,
+      message: "Store Deleted Successfully",
+    });
+  });
+
+  export const UpdateStore = catchAsyncError(async (req, res, next) => {
+    const {name,category,phonenumber,website,details,videourl,latitude,longitude} = req.body;
+    const store = await Store.findById(req.params.id);
+    if (name) store.name = name;
+    if (category) store.category = category;
+    if (phonenumber) store.phonenumber = phonenumber;
+    if (website) store.website = website;
+    if (details) store.details = details;
+    if (videourl) store.videourl = videourl;
+    if (latitude) store.latitude = latitude;
+    if (longitude) store.longitude = longitude;
+    await store.save();
+    res.status(200).json({
+      success: true,
+      message: "Store Updated Successfully",
+      store
+    });
+  });
+
+
+  export const UpdateStoreProfile= catchAsyncError(async (req, res, next) => {
+    const store = await Store.findById(req.params.id);
+    if(req.files['storegalleryfiles'][0]){
+    const file = getDataUri(req.files['storegalleryfiles'][0]);
+    await cloudinary.v2.uploader.destroy(store.storegallery.public_id);
+    const mycloud = await cloudinary.v2.uploader.upload(file.content);
+    store.storegallery ={
+      public_id:mycloud.public_id,
+      url:mycloud.secure_url,
+    }
+    }
+   if(req.files['storephotofiles'][0]){
+      const file = getDataUri(req.files['storephotofiles'][0]);
+      await cloudinary.v2.uploader.destroy(store.storephoto.public_id);
+      const mycloud = await cloudinary.v2.uploader.upload(file.content);
+      store.storephoto ={
+        public_id:mycloud.public_id,
+        url:mycloud.secure_url,
+      }
+      }
+    await store.save();
+    res.status(200).json({
+      success: true,
+      message: "Store Images  Updated Successfully",
+    });
+  });
+
+
+
--- routes/newStoreRoutes.js
+++ routes/newStoreRoutes.js
@@ -1,8 +1,11 @@
 import express from "express";
-import { AddnewStore } from "../controllers/managestoreController.js";
+import { AddnewStore, DeleteStore, GetAllStores, UpdateStore, UpdateStoreProfile } from "../controllers/managestoreController.js";
 import { isAuthenticated ,authorizeAdmin} from "../middlewares/auth.js";
 import { storeUpload } from "../middlewares/multer.js";
 const router = express.Router();
 router.route("/addnewstore").post(isAuthenticated,authorizeAdmin,storeUpload,AddnewStore);
+router.route("/allstores").get(GetAllStores);
+router.route("/stores/:id").delete(DeleteStore).put(UpdateStore);
+router.route("/updatestoreimage/:id").put(storeUpload,UpdateStoreProfile);
 // ResetPassword
 export default router;
