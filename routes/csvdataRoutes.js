71b3940d08a4b18f9cc23452f34227ce9896c333
Adarsh_the_Developer <adarsh_the_developer@adarshs-MacBook-Pro.local>
2023-05-15 10:52:17 +0530

    single csv data fixed

--- routes/csvdataRoutes.js
+++ routes/csvdataRoutes.js
@@ -1,9 +1,17 @@
 import express from "express";
-import { Eventcsvdata, Offercsvdata, Storecsvdata, usercsvdata } from "../controllers/csvdataController.js";
+import { Eventcsvdata, Messagescsvdata, Offercsvdata, SingleEventCsvData, SingleOfferCsvData, SingleStoreCsvData, SingleUserCsvData, Storecsvdata, usercsvdata } from "../controllers/csvdataController.js";
 const router = express.Router();
 router.route("/getusercsvdata").get(usercsvdata);
 router.route('/getstorecsvdata').get(Storecsvdata);
 router.route('/getoffercsvdata').get(Offercsvdata);
 router.route("/geteventscsvdata").get(Eventcsvdata);
+router.route("/getallusermessagescsvdata").get(Messagescsvdata)
+//single csv data routes 
+router.route("/getusercsvdata/:id").get(SingleUserCsvData);
+router.route("/getstorecsvdata/:id").get(SingleStoreCsvData);
+router.route("/getoffercsvdata/:id").get(SingleOfferCsvData)
+router.route('/geteventscsvdata/:id').get(SingleEventCsvData);
+
+
 
 export default router; 
