66192f35c897256ff5bb5e52237037a4e8fc3596
Adarsh_the_Developer <adarsh_the_developer@adarshs-MacBook-Pro.local>
2023-05-30 19:02:34 +0530

    fafaf

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 // }))
 app.use(
 	cors({
-		origin:"https://demo7.progressiveaidata.in/",
+		origin:"https://demo7.progressiveaidata.in",
 		credentials: true,
 		methods: ["GET", "POST", "PUT", "DELETE"],
 	})
