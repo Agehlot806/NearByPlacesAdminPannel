38a3adc870475ebe854f2a6c226cd8da55ba4514
Adarsh_the_Developer <adarsh_the_developer@adarshs-MacBook-Pro.local>
2023-05-31 10:33:37 +0530

    localurladdedto origin cors

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 // }))
 app.use(
 	cors({
-		origin:"https://demo7.progressiveaidata.in",
+		origin:["https://demo7.progressiveaidata.in","http://localhost:3000"],
 		credentials: true,
 		methods: ["GET", "POST", "PUT", "DELETE"],
 	})
