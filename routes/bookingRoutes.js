989918ec60ee75df33b72a30692bfd6f97ad0a15
Adarsh_the_Developer <adarsh_the_developer@adarshs-MacBook-Pro.local>
2023-06-06 18:26:45 +0530

    fixed

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -3,7 +3,7 @@ import { isAuthenticated } from "../middlewares/auth.js";
 import { NewBooking, getAllBookings, getSingleBooking, myBookings, updateBookingStatus } from "../controllers/bookingController.js";
 const router  = express.Router();
 router.route("/booking/new").post(isAuthenticated,NewBooking);
-router.route("/booking/:id").get(isAuthenticated,getSingleBooking);
+router.route("/booking/:id").get(getSingleBooking);
 router.route("/booking/me").get(isAuthenticated,myBookings);
 router.route("/admin/bookings").get(getAllBookings);
 router.route("/booking/update").put(updateBookingStatus)
