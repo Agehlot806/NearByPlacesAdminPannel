293ea9d6c205f702e4628875cb8225fb18ce0d09
Ashok Gehlot <107103570+Agehlot806@users.noreply.github.com>
2023-06-21 16:07:38 +0530

    Update newStoreRoutes.js

--- routes/newStoreRoutes.js
+++ routes/newStoreRoutes.js
@@ -2,11 +2,11 @@ import express from "express";
 import { AddnewStore, DeleteStore, GetAllStores, UpdateStore, createStoreReviews, getStoreReview,deleteReview, getStoreBYId, GetAllStoresRecentAdded, getAllStoreReviews, subscriptionPlanForMerchant, getallSubscription, updateSubscription, getallSubscriptionById, deleteSubscription,paymentVerificationofSubscription,checkousubscription} from "../controllers/managestoreController.js";
 import { isAuthenticated } from "../middlewares/auth.js";
 const router = express.Router();
-router.route("/addnewstore/:categoryId/stores").post(AddnewStore);
+router.route("/addnewstore/:categoryId/stores").post(isAuthenticated,AddnewStore);
 router.route("/checkout/:id").post(checkousubscription)
 router.route("/subscriptionverification").post(paymentVerificationofSubscription);
 router.route("/allstores").get(GetAllStores);
-router.route("/stores/:id").delete(DeleteStore).put(UpdateStore);
+router.route("/stores/:id").delete(isAuthenticated,DeleteStore).put(isAuthenticated,UpdateStore);
 router.route("/review").put(isAuthenticated, createStoreReviews);
 router.route("/reviews").get(getStoreReview);
 router.route("/allstores/:id").get(getStoreBYId)
