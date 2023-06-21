69bd076f2dfbe5ef78b0e719b2a9fee377d74041
Ashok Gehlot <107103570+Agehlot806@users.noreply.github.com>
2023-06-21 12:11:34 +0530

    Update eventRoutes.js

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -1,16 +1,17 @@
 import express from "express";
-import { AddnewEvent, DeleteEventById, GetAllEvents, UpdateEvent, addparticipantToEvent, deleteparticipantfromEvent, getEventById ,getallEventParticipants} from "../controllers/eventController.js";
+import { AddnewEvent,permissongive, permissiontake,DeleteEventById, GetAllEvents, UpdateEvent, addparticipantToEvent, deleteparticipantfromEvent, getEventById ,getallEventParticipants} from "../controllers/eventController.js";
 import {isAuthenticated, specificauth } from "../middlewares/auth.js";
 import { authorizeAdmin } from "../middlewares/auth.js";
 const router = express.Router();
-router.route("/createevent").post(AddnewEvent);
-router.route('/allevents').get(isAuthenticated,authorizeAdmin,specificauth,GetAllEvents);
+router.route("/createevent").post(isAuthenticated, AddnewEvent);
+router.route('/allevents').get(isAuthenticated,GetAllEvents);
 router.route("/allevent/:id").get(getEventById);
-router.route("/events/:id").put(isAuthenticated,authorizeAdmin,UpdateEvent).delete(isAuthenticated,authorizeAdmin,DeleteEventById);
+router.route("/events/:id").put(isAuthenticated,authorizeAdmin,UpdateEvent).delete(isAuthenticated,DeleteEventById);
 router.route("/addparticipanttoevent").put(isAuthenticated,addparticipantToEvent);
 router.route("/allparticipant").get(getallEventParticipants);
 router.route("/allparticipant").delete(isAuthenticated,deleteparticipantfromEvent);
 
-
+router.route("/permissongive").post(isAuthenticated, permissongive);
+router.route("/permissiontake").post(isAuthenticated,permissiontake);
 
 export default router; 
