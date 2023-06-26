13afc947d238977c34f55dfb46aa4f1d3cebde93
Ashok Gehlot <107103570+Agehlot806@users.noreply.github.com>
2023-06-26 11:38:06 +0530

    Update reservationRoutes.js

--- routes/reservationRoutes.js
+++ routes/reservationRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { ReservationCheckout, getallordersofReservation, paymentVerificationofReservation } from "../controllers/ResevationController.js";
+import { ReservationCancellation, ReservationCheckout, getallordersofReservation, paymentVerificationofReservation } from "../controllers/ResevationController.js";
 import { isAuthenticated } from "../middlewares/auth.js";
 const router = express.Router();
 router.route("/reservation-checkout/:id").post(isAuthenticated,ReservationCheckout);
 router.route("/paymentverificationofreservation").post(paymentVerificationofReservation);
 router.route("/getallreservation").get(getallordersofReservation)
+router.route("/cancelledreservation/:reservationId").post(ReservationCancellation)
+
 
 
 export default router;
