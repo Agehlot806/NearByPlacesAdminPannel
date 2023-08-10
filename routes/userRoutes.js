801ab6aacddc31be7b0a9ada05472adbb9a14016
Ashok Gehlot <107103570+Agehlot806@users.noreply.github.com>
2023-08-10 13:01:10 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,14 @@
 import express from "express";
-import {registerUser,login,logout,getMyProfile, updateProfile,updatePassword,forgetPassword,resetPassword, testapi, updateUserRole, getallusers, SendNotification, merchantRegister, sendEmailtoAll, getUserfavoriateResturent, removeUserFavoriteRestaurant, countUserLikedRestaurants} from "../controllers/adminController.js";
+import {PhoneOtp,verify,getMyProfile,logout,registerUser, updateProfile,updatePassword,forgetPassword,resetPassword, testapi, updateUserRole, getallusers, SendNotification, merchantRegister, sendEmailtoAll, getUserfavoriateResturent, removeUserFavoriteRestaurant, countUserLikedRestaurants} from "../controllers/adminController.js";
 import {authorizeAdmin, isAuthenticated } from "../middlewares/auth.js";
 import passport from "../utils/strategy.js";
 import { getGraphData } from "../controllers/dashboardController.js";
 // import passport from "passport";
 const router = express.Router();
-router.route("/registeradmin").post(registerUser);
-router.route("/login").post(login);
+// router.route("/registeradmin").post(registerUser);
+router.route("/PhoneOtp").post(PhoneOtp);
+router.route("/verify").post(verify);
+router.route("/registerUser").post(registerUser);
 router.route("/logout").get(logout);
 router.route("/myprofile").get(isAuthenticated,getMyProfile);
 router.route("/updateadminpassword").put(isAuthenticated,updatePassword);
