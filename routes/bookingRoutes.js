81d381dba344fb561ec4e5cb3ed88ab0e665452c
Ashok Gehlot <107103570+Agehlot806@users.noreply.github.com>
2023-08-16 18:37:32 +0530

    Update bookingRoutes.js

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { authorizeAdmin, isAuthenticated } from "../middlewares/auth.js";
-import { NewBookingUser,deletebooking,getAllBookings, getAllBookingsAllStore, getBooking, getUserBookings } from "../controllers/bookingController.js";
+import { NewBookingUser,NewBookingByUser,deletebooking,getAllBookings, getAllBookingsAllStore, getBooking, getUserBookings } from "../controllers/bookingController.js";
 
 
 const router  = express.Router();
 router.route("/newbooking/:storeId/table/:tableId").post(isAuthenticated,NewBookingUser);
+router.route("/newbookingByUser/:storeId/table/:tableId").post(NewBookingByUser);
 router.route("/allbooking/:storeId").get(isAuthenticated,authorizeAdmin,getAllBookings);
 router.route("/mybooking").get(isAuthenticated,getUserBookings);
 router.route("/getbooking/:storeId/:bookingId").get(isAuthenticated,getBooking);
